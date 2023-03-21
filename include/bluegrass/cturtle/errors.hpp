b3a06ca1eb550929960b9024135d8d6f3a00115b
Bucky Kittinger <bucky.kittinger@eosnetwork.com>
2023-03-20 22:34:07 -0400

    more string_view

--- include/bluegrass/cturtle/errors.hpp
+++ include/bluegrass/cturtle/errors.hpp
@@ -9,14 +9,14 @@
 
 namespace bluegrass::cturtle {
    struct error {
-      constexpr inline error(std::string_view msg, std::size_t ec)
-         : error_msg(msg), error_code(ec) {}
+      inline error(std::string msg, std::size_t ec)
+         : error_msg(std::move(msg)), error_code(ec) {}
       template <std::size_t N>
       constexpr inline error(const char (&msg)[N], std::size_t ec)
          : error_msg(msg, N-1), error_code(ec) {}
-      constexpr inline std::string_view msg() const noexcept { return error_msg; }
+      constexpr inline const std::string& msg() const noexcept { return error_msg; }
       constexpr inline std::size_t code() const noexcept { return error_code; }
-      std::string_view error_msg;
+      std::string error_msg;
       std::size_t error_code;
    };
 
